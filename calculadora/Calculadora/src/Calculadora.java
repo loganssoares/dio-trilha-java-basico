c683227e800eec41cc4614e307704816c95580e8
Logans <loganshp@hotmail.com>
2024-07-29 12:19:08 +0100

    Estudo sobre comentários e documentação.

---
+++ calculadora/Calculadora/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora<h1>
+ * The calculator do math operations between integer numbers
+ * <p>
+ * <b> Note:<b> Read carefully this class documentation to enjoy the resouces offer by author
+ * 
+ * @author Logans Soares
+ * @version 1.0
+ * @since 2024
+*/
+
+public class Calculadora {
+    /**
+     * This method is used to sum two integer numbers
+     * @param numeroUm this is the first method param
+     * @param numeroDois this is the second method param
+     * @return int the result this method is the add of the two numbers
+     */
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ calculadora/Calculadora/src/Comentarios.java
@@ -0,0 +1,33 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Hello, I am a comment in a single line
+
+        /*Hello,
+         * I am a comment
+         * which can be more detail
+         * when necessary
+         */
+    }
+    /**
+     * this two litle stars above
+     * is to identify that you
+     * pretender elaborate a comment
+     * in a document level.
+     * thart's amazing
+     */
+    public void metodo(){
+
+    }
+
+
+public int somaMultplica (int n, int x, String m){
+    int r = 0; // r is equal the result
+    if (m == "M"){// M = multiplication
+        r = n * x;
+    }else{
+        // if not, sum.
+        r = n + x;
+    }
+    return r;
+}
+}
